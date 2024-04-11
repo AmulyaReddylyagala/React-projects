fde9eddfbaf81de11f0118e6920808afed5781ad
Amulya Reddy <amulyareddy051@gmail.com>
2024-04-11 21:52:53 +0530

    added projects screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import { useState } from 'react';
 import './App.css'
 import ColorFlipper from './Components/ColorFlipper/ColorFlipper'
 import DataFetching from './Components/DataFetching/DataFetching'
 
 function App() {
+
+  const [project, setProject] = useState(0);
+
+  if (project == 1) {
+    return <ColorFlipper />
+  }
+  if (project == 2)
+    return <DataFetching />
   return <>
-    {/* <DataFetching /> */}
-    <ColorFlipper />
+    <div className='mainDiv'>
+      <h2 className='title'>Projects</h2>
+      <div className='list'>
+        <div className='projects' onClick={() => setProject(1)}>
+          Color Filpper
+        </div>
+        <div className='projects' onClick={() => setProject(2)}>
+
+          Data Feteching
+        </div>
+      </div>
+    </div>
   </>
 }
 
